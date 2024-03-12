81862c256d525ae5cc8cc02a49f85c6501eb1f41
Arthur Wong <a.wong45789@gmail.com>
2024-03-12 11:41:10 +1100

    dynamic catalogue categories from api

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const catalogue = require('./routes/catalogue')
+const catalogueFilters = require('./routes/catalogue-filter')
 const app = express()
 const port = 3001
 const dotenv = require('dotenv').config()
 app.use('/catalogue',catalogue)
+app.use('/catalogue-filters',catalogueFilters)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ routes/catalogue-filter.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var fs = require("fs/promises");
+const getDB = require("../database");
+var router = express.Router();
+
+router.get("/", async function (req, res) {
+  let db = await getDB();
+  let collection = await db.collection("catalogue_filters");
+  let results = await collection.find({}).limit(50).toArray();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.send(results).status(200);
+});
+
+module.exports = router;
