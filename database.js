2ade31ec05c851932da8d92d78fa12ba1aaca903
Arthur Wong <a.wong45789@gmail.com>
2024-03-14 15:08:07 +1100

    simple catalogue search

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require("mongodb");
-
+let client;
 const getDB = async () => {
   const connectionString = process.env.DB_URI || "";
-  const client = new MongoClient(connectionString);
+  client = new MongoClient(connectionString);
   let conn;
   try {
     conn = await client.connect();
@@ -11,5 +11,4 @@ const getDB = async () => {
   }
   return conn.db("gear_store");
 };
-
 module.exports = getDB;
--- routes/catalogue.js
+++ routes/catalogue.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var fs = require("fs/promises");
 const getDB = require("../database");
+const closeClient = require("../database");
 var router = express.Router();
 
 router.get("/", async function (req, res) {
   let db = await getDB();
   let collection = await db.collection("catalogue");
-  let results = await collection.find({}).limit(50).toArray();
+  const query = req.query.q;
+  const regex = new RegExp(`${query}`, "i")
+  let results = await collection.find({name: regex}).limit(50).toArray();
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send(results).status(200);
